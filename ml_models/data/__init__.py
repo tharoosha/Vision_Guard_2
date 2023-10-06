8a3c96f5e257207a3d51b617b6abbc05da12fc6f
tharoosha <tharooshavihidun@gmail.com>
2023-10-06 13:54:05 +0530

    transforms init

---
+++ ml_models/data/__init__.py
@@ -0,0 +1,3 @@
+from dataset_prep import ARID_prep
+
+__all__ = ('ARID_prep')
--- ml_models/train_model.py
+++ ml_models/train_model.py
@@ -1,18 +1,73 @@
 from data import flow_iterator
 from data.dataset_prep.ARID_prep import ARID_prep
+import argparse
+import csv
+import os
+
+from tensorboardX import SummaryWriter
 
 
-class Model(object):
-    def __init__(self, vid_path):
-        self.flow = flow_iterator.Flow(vid_path)
+parser = argparse.ArgumentParser(description='PyTorch Two-Stream Action Recognition')
 
-    def process(self):
-        # process the flow frames here
-        # for frame in self.flow:
-        #     # do something with the frame
-        #     pass
-        frame_count = self.flow.count_frames()
-        return frame_count
+
+
+# class Model(object):
+#     def __init__(self, vid_path):
+#         self.flow = flow_iterator.Flow(vid_path)
+
+#     def process(self):
+#         # process the flow frames here
+#         # for frame in self.flow:
+#         #     # do something with the frame
+#         #     pass
+#         frame_count = self.flow.count_frames()
+#         return frame_count
+    
+def main():
+    global args, best_prec1, model, writer, best_loss, length, width, height, input_size, scheduler, suffix
+    args = parser.parse_args()
+
+    training_continue = args.contine
+    if not args.no_attention:
+        args.arch='dark_light_noAttention'
+
+    suffix = 'ga=%s_b=%s_both_flow=%s' % (args.gamma , args.batch_size , args.both_flow)
+    headers = ['epoch', 'top1', 'top5', 'loss']
+    with open('train_record_%s.csv' % suffix, 'w', newline='') as f:
+        record = csv.writer(f)
+        record.writerow(headers)
+
+    with open('validate_record_%s.csv' % suffix, 'w', newline='') as f:
+        record = csv.writer(f)
+        record.writerow(headers)
+
+    print('work in both_flow = %s, gamma = %s, batch_size = %s'%(args.both_flow, args.gamma, args.batch_size))
+    
+    input_size = 112
+    width = 170
+    height = 128
+
+    saveLocation="./checkpoint/"+args.dataset+"_"+args.arch+"_split"+str(args.split)
+    if not os.path.exists(saveLocation):
+        os.makedirs(saveLocation)
+    writer = SummaryWriter(saveLocation)
+
+    # create model
+
+    if args.evaluate:
+        print("Building validation model ... ")
+        model = build_model_validate()
+        optimizer = AdamW(model.parameters(), lr= args.lr, weight_decay=args.weight_decay)
+    elif training_continue:
+        model, startEpoch, optimizer, best_prec1 = build_model_continue()
+        for param_group in optimizer.param_groups:
+            lr = param_group['lr']
+        print("Continuing with best precision: %.3f and start epoch %d and lr: %f" %(best_prec1,startEpoch,lr))
+    else:
+        print("Building model with ADAMW... ")
+        model = build_model()
+        optimizer = AdamW(model.parameters(), lr= args.lr, weight_decay=args.weight_decay)
+        startEpoch = 0
 
 if __name__ == '__main__':
     vid_path = 'ml_models/dataset/ARID_v1.5/clips_v1.5_avi/Drink/Drink_1_1.avi'
