d1ce8e9d245632d71c624a4304bcedbf45184443
tharoosha <tharooshavihidun@gmail.com>
2023-10-13 17:23:25 +0530

    model building

---
+++ ml_models/models/BERT/embedding/position.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+import torch
+import math
+
+device = 'mps' if torch.backends.mps.is_available() else "cpu"
+
+    
+class LearnedPositionalEmbedding2(nn.Module):
+
+    def __init__(self, d_model, max_len=512):
+        super().__init__()
+
+        # Compute the positional encodings once in log space.
+        # pe = torch.zeros(max_len, d_model).float().to(device='cuda')
+        pe = torch.zeros(max_len, d_model).float().to(device='mps')
+        pe.require_grad = True
+        pe = pe.unsqueeze(0)
+        self.pe=nn.Parameter(pe)
+        torch.nn.init.normal_(self.pe,std=0.02)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)]
+
+    
--- ml_models/opt/__init__.py
+++ ml_models/opt/__init__.py
@@ -1,1 +1,2 @@
 from .AdamW import AdamW
+
